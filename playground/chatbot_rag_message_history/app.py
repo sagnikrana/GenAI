f48645a012bf6729297b3dadabd91dbd8f26d65f
Sagnik Rana <sagnik.rana1994@gmail.com>
2025-08-03 16:21:55 -0400

    Update app.py

--- playground/chatbot_rag_message_history/app.py
+++ playground/chatbot_rag_message_history/app.py
@@ -1,4 +1,32 @@
 import streamlit as st
 
-st.title("Hello, Streamlit!")
+# Interface for user to submit the PDF
 
+# Read the PDF
+
+# Store it into the Chroma DB interface
+
+# Tell user that it's ready
+
+# User interface to submit question 
+
+# Take the info and do a similarity search
+
+# Validate the output
+
+# return the output to the user.
+
+
+
+
+
+# Turn the user search answer box to a query based interface
+
+
+# Provide users option to select the temperatire and other objectives. 
+
+
+# Mention how much of chat history we need to provide.
+
+
+# Handle exceptions and prepare for shipping
