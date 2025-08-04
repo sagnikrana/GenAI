89ac04b3788a0a717cf6fccb8e004e025576e4e9
Sagnik Rana <sagnik.rana1994@gmail.com>
2025-08-03 22:35:51 -0400

    Updating files

--- playground/chatbot_rag_message_history/app.py
+++ playground/chatbot_rag_message_history/app.py
@@ -1,12 +1,60 @@
 import streamlit as st
+import time
+
+from langchain.document_loaders import PyPDFLoader
+from langchain_community.embeddings import OllamaEmbeddings
+from langchain_chroma import Chroma
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
 
 # Interface for user to submit the PDF
 
-# Read the PDF
+uploaded_file = st.file_uploader("Upload your PDF file", type="pdf")
 
-# Store it into the Chroma DB interface
+
+# Read the PDF
+documents = None
+vector_db = None
+
+if uploaded_file :
+    st.write("treating the uploaded file")
+    # Step 2: Save the uploaded file temporarily (PyPDFLoader can work with file paths)
+    with open("temp_uploaded.pdf", "wb") as f:
+        f.write(uploaded_file.getbuffer())
+
+    # Step 3: Load the PDF file using PyPDFLoader from the saved path
+    loader = PyPDFLoader("temp_uploaded.pdf")
+    documents = loader.load()
+    st.write("here")
+
+# Get the embeddings and store it into the Chroma DB interface
+
+embeddings=(OllamaEmbeddings(model="gemma:2b"))  ##by default it ues llama2
+
+
+# Split
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=100)
+
+start = time.time()
+
+if documents:
+    st.write("Splitting the documents and persisting ")
+    splits = text_splitter.split_documents(documents)
+    vector_db = Chroma.from_documents(splits,embeddings, persist_directory = '..\stored_data\chroma_db')
+    st.write("Persisted")
+    # Process 1
+    end = time.time()
+    print(f"Process 1 took {end - start} seconds")
+
+
+
+# Write code to summarize the document using the Ollama Embedding 
 
 # Tell user that it's ready
+if vector_db:
+    st.write("the data is ready !!")
+    name = st.text_input('Please enter your question about the document')
+
 
 # User interface to submit question 
 
@@ -15,6 +63,7 @@ import streamlit as st
 # Validate the output
 
 # return the output to the user.
+
 
 
 
