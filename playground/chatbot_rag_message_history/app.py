d04dcd22c4eca862bd29c065050603978c4c1ead
Sagnik Rana <sagnik.rana1994@gmail.com>
2025-09-01 19:55:57 -0400

    Updating files

--- playground/chatbot_rag_message_history/app.py
+++ playground/chatbot_rag_message_history/app.py
@@ -16,6 +16,7 @@ from langchain_community.document_loaders import WebBaseLoader
 from langchain_core.documents import Document
 from langchain_openai import OpenAIEmbeddings
 from langchain_openai import ChatOpenAI
+from langchain_community.document_loaders.notebook import remove_newlines
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.messages import HumanMessage,SystemMessage
@@ -36,6 +37,7 @@ uploaded_file = st.file_uploader("Upload your PDF file", type="pdf")
 # Read the PDF
 documents = None
 vector_db = None
+cleaned_docs = None
 
 embeddings=(OllamaEmbeddings(model="gemma:2b"))  ##by default it ues llama2. gemma:2b is downloaded in local pc
 llm = Ollama(model="gemma:2b")
@@ -50,7 +52,6 @@ if uploaded_file :
     # Step 3: Load the PDF file using PyPDFLoader from the saved path
     loader = PyPDFLoader("temp_uploaded.pdf")
     documents = loader.load()
-    st.write("here")
 
 # Get the embeddings and store it into the Chroma DB interface
 
@@ -71,41 +72,31 @@ if documents:
     # Process 1
     end = time.time()
     st.write(f"Process 1 took {end - start} seconds")
+        
+    cleaned_docs = [
+        Document(page_content=remove_newlines(doc.page_content), metadata=doc.metadata)
+        for doc in splits
+    ]
+    st.write("Cleaned docs are ready.")
+
 
 
 # Write code to summarize the document using the Ollama Embedding
-
 prompt=ChatPromptTemplate.from_template(
     """
 Answer the following question based only on the provided context:
 <context>
 {context}
 </context>
-
 """
 )
 
 document_chain=create_stuff_documents_chain(llm,prompt)
 response = document_chain.invoke({
-   # "input" : "What is the biggest fault in the boeing aircrafts as per the article?",
-   "input" : "What happened with the 737 max aircrafts?",
+   "input" : "Please summarize in short the document that's provided to you",
     "context" :cleaned_docs
 })
-
-messages=[
-    SystemMessage(content="Answer about the problems that boeing has faced in the recent times"),
-    HumanMessage(content="What challenges Boeing 737 max has faced in recent times?")
-]
-
-parser=StrOutputParser()
-# parser.invoke(result)
-
-chain=llm|parser
-
-# Invoke contains the list about the messages. Could be i/p or many forms of messagyes, system, human, AI etc..
-chain.invoke(messages)
-
-
+st.write("Here is the response : {response}")
 
 # Tell user that it's ready
 if vector_db:
